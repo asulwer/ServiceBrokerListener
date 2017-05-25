a60f99513b3b6c760106f2995b2c46b10510ae6a
Aaron Sulwer <asulwer@gmail.com>
2017-05-25 11:48:02 -0500

    Update SqlDependencyEx.cs

--- ServiceBrokerListener/ServiceBrokerListener.Domain/SqlDependencyEx.cs
+++ ServiceBrokerListener/ServiceBrokerListener.Domain/SqlDependencyEx.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
-namespace DurableAD.Helpers
+namespace ServiceBrokerListener.Domain
 {
     public sealed class SqlDependencyEx : IDisposable
     {
